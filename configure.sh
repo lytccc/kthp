b3517766df3dad8165707c01414f62c7888efe85
lytccc <lytccc@users.noreply.github.com>
2020-09-15 18:29:07 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,8 +6,8 @@ curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-install -m 666 /tmp/v2ray/geoip.dat /usr/local/lib/v2ray/geoip.dat
-install -m 666 /tmp/v2ray/geosite.dat /usr/local/lib/v2ray/geosite.dat
+install -m 666 /tmp/v2ray/geoip.dat /usr/local/bin/geoip.dat
+install -m 666 /tmp/v2ray/geosite.dat /usr/local/bin/geosite.dat
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
